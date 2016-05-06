c54ff3a6c268489697dd1e9c4915b8328f9e6561
shooga <avishag212@gmail.com>
2016-05-06 18:22:27 +0300

    added Alice class findkey() isn't implemented.Waiting for the tree

--- src/Alice.java
+++ src/Alice.java
@@ -7,6 +7,7 @@ public class Alice {
     //Add Fields if needed
 
     public Alice(){
+    	
     }
 
     public Puzzle[] getPuzzlesCopy(){
@@ -18,14 +19,38 @@ public class Alice {
     }
 
     public void createPuzzles(int n, int k){
-        //Complete Your Code Here
+    	puzzlesArray=new Puzzle[k];
+    	//cast lots each number in the arrays
+    	for (int j=0 ; j<k ; j++){
+    		puzzlesArray[j]= new Puzzle(RandomAnArray(puzzlesArray[j].getRiddle()),RandomAnArray(puzzlesArray[j].getPrivateKey()));
+    	
+    	}
+    	
     }
 
     public Pair<String, Integer> findKey(String sIndex){
         //Complete Your Code Here
     }
 
-
+    public int[] RandomAnArray(int[] arr){
+    	int N=arr.length; 
+    	int randomIndx,tmp;
+    	
+    	// random a number in range [(i-1)N^3,iN^3) for each cell
+    	for (int i=0;i<N;i++){
+    		arr[i]=(int)(Math.random()*(N^3)+((i-1)*N^3));
+    	}
+    	// shuffle
+    	for(int i=0;i<N;i++){
+    		randomIndx=(int)Math.random()*N +i;
+    		
+    		tmp=arr[randomIndx];
+    		arr[randomIndx]=arr[i];
+    		arr[i]=tmp;
+ 
+    	}
+    	return arr;
+    }
 
 
 }
